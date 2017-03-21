02af2550300eb62560b138575107919ed056a360
yoneal <yoneal@gmail.com>
2017-03-21 21:54:12 +0800

    NoteService unit test

--- src/app/note.service.spec.ts
+++ src/app/note.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject, async} from '@angular/core/testing';
 
 import { NoteService } from './note.service';
 
@@ -9,7 +9,27 @@ describe('NoteService', () => {
     });
   });
 
-  it('should ...', inject([NoteService], (service: NoteService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('get1SecNote() should receive a note after 1 second of delay', async(inject([NoteService], (service: NoteService) => {
+    let start = new Date();
+    service.sendNote("1 second");
+    return service.get1SecNote().subscribe((note: string) => {
+      let end = new Date();
+      let elapsed = end.getTime() - start.getTime();
+      expect(elapsed).toBeGreaterThanOrEqual(1000);
+      expect(elapsed).toBeLessThan(1010); // around a second
+      expect(note).toBe("1 second");
+    });
+  })));
+
+  it('get2SecNote() should receive a note after 2 seconds of delay', async(inject([NoteService], (service: NoteService) => {
+    let start = new Date();
+    service.sendNote("2 seconds");
+    return service.get2SecNote().subscribe((note: string) => {
+      let end = new Date();
+      let elapsed = end.getTime() - start.getTime();
+      expect(elapsed).toBeGreaterThanOrEqual(2000);
+      expect(elapsed).toBeLessThan(2010);
+      expect(note).toBe("2 second");
+    });
+  })));
 });
