cf5b60c6126927d5c6942f3c085f0f9c0d07cd05
yoneal <yoneal@gmail.com>
2017-03-22 14:50:27 +0800

    Add end-to-end tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { SupportPage } from './app.po';
+import {by, browser, ExpectedConditions} from "protractor";
 
 describe('support App', () => {
   let page: SupportPage;
@@ -7,8 +8,56 @@ describe('support App', () => {
     page = new SupportPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display a list with initial contents', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    let list = page.getList();
+    expect(list.element(by.css('h3')).getText()).toEqual('Notes:');
+    let notes = page.getNotes();
+    notes.each((elem, index) => {
+      expect(elem.getText()).toEqual(`Note ${index+1}`);
+    });
+  });
+
+  it('should display a button with q&a icon', () => {
+    page.navigateTo();
+    let button = page.getButton();
+    expect(button.element(by.css('md-icon')).getText()).toEqual('question_answer');
+  });
+
+  it('clicking the button should display a dialog', () => {
+    page.navigateTo();
+    let button = page.getButton();
+    button.click();
+    expect(page.getNoteDialog()).toBeTruthy();
+  });
+
+  it('creating a note should update the list in the dialog', () => {
+    page.navigateTo();
+    let button = page.getButton();
+    button.click();
+    let input = page.getNoteInput();
+    input.click();
+    input.$('input').sendKeys('Note 3');
+    let sendBtn = page.getNoteSendBtn();
+    sendBtn.click();
+    browser.wait(ExpectedConditions.presenceOf(page.getNoteNotes().get(2)), 1010);
+    let notes = page.getNoteNotes();
+    expect(notes.count()).toEqual(3);
+    expect(notes.last().getText()).toEqual('Note 3');
+  });
+
+  it('creating a note should update the list in the main page', () => {
+    page.navigateTo();
+    let button = page.getButton();
+    button.click();
+    let input = page.getNoteInput();
+    input.click();
+    input.$('input').sendKeys('Note 3');
+    let sendBtn = page.getNoteSendBtn();
+    sendBtn.click();
+    browser.wait(ExpectedConditions.presenceOf(page.getNotes().get(2)), 2010);
+    let notes = page.getNotes();
+    expect(notes.count()).toEqual(3);
+    expect(notes.last().getText()).toEqual('Note 3');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,44 @@
-import { browser, element, by } from 'protractor';
+import {browser, element, by, $} from 'protractor';
 
 export class SupportPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getList() {
+    return $('app-root > md-list');
+  }
+
+  getNotes() {
+    return this.getList().all(by.css('md-list-item'));
+    // return element.all(by.css('app-root > md-list > md-list-item'));
+  }
+
+  getButton() {
+    return $('app-root > button.support');
+  }
+
+  getNoteDialog() {
+    return $('.cdk-overlay-container app-note-dialog');
+  }
+
+  getNoteInput() {
+    let dialog = this.getNoteDialog();
+    // return dialog.element(by.css('.mat-dialog-actions md-input-container'));
+    return dialog.$('.mat-dialog-actions').$('md-input-container');
+  }
+
+  getNoteSendBtn() {
+    let dialog = this.getNoteDialog();
+    return dialog.$('.mat-dialog-actions').$('button');
+  }
+
+  getNoteList() {
+    let dialog = this.getNoteDialog();
+    return dialog.$('.mat-dialog-content').$('md-list');
+  }
+
+  getNoteNotes() {
+    return this.getNoteList().all(by.css('md-list-item'));
   }
 }
